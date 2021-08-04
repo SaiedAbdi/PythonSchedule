27e9174e8e1082b4bdbec74acf1de2ba458456e9
saied <saied.programmer@gmail.com>
2021-08-04 16:45:22 +0430

    add schedule project created with Python

---
+++ __init__.py
@@ -0,0 +1,20 @@
+import main
+import time
+
+
+def show(name):
+    print(f'Hello {name}')
+
+
+def greeting():
+    print('How are you?')
+
+
+main.every(6).seconds.do(show, 'jack')
+main.every(4).seconds.do(greeting)
+
+
+while True:
+    main.run_pending()
+    print(main.idle_seconds())
+    time.sleep(1)
